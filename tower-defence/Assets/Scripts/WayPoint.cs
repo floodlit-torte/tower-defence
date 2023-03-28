6aa493e08b4dfac5ca5992432c605086055200cc
floodlit-torte <arkeyalibi171007@gmail.com>
2023-03-28 17:50:54 +0600

    Tower realisation

--- tower-defence/Assets/Scripts/WayPoint.cs
+++ tower-defence/Assets/Scripts/WayPoint.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
-
+    [SerializeField] private bool isPlaycable;
+    [SerializeField] private GameObject Tower;
+    private void OnMouseDown()
+    {
+        if (!isPlaycable)
+            return;
+        Instantiate(Tower, gameObject.transform.position, Quaternion.identity);
+        isPlaycable = false;
+    }
 }
